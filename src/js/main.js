d0b70652d82a23c8bd9dfbe09d816da6b1fbc40b
Artur <artur.czerwiak@gmail.com>
2024-09-30 21:33:16 +0200

    Create footer and subpage contact

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const navMenu = document.querySelector('.nav__menu');
 const menuLink = document.querySelectorAll('.nav__menu a');
+const copyrightDateSpan = document.querySelector('.copyright-date');
 
 const showMobileMenu = () => {
 	navMenu.classList.add('nav__mobile-menu');
@@ -10,8 +11,15 @@ const hideMobileMenu = () => {
 	navMenu.classList.remove('nav__mobile-menu');
 };
 
+const setCurrentYear = () => {
+	const year = new Date().getFullYear();
+	copyrightDateSpan.textContent = year;
+};
+
 menuLink.forEach((element) => {
 	element.addEventListener('click', hideMobileMenu);
 });
 
 burgerBtn.addEventListener('click', showMobileMenu);
+
+setCurrentYear();
